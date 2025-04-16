30e51ff47beb8007748af9ea135ada9152c238ad
mwalzer <walzer@ebi.ac.uk>
2025-04-16 17:04:31 +0000

    fixed non-omission of null values in CvParameter serialisation

--- MZQC-module.cs
+++ MZQC-module.cs
@@ -165,7 +165,7 @@ namespace MzqcCsLib
         public string Name { get; set; }
 
         /// Value of the parameter.
-        [JsonProperty("value")]
+        [JsonProperty("value", NullValueHandling = NullValueHandling.Ignore)]
         public object Value { get; set; }
     }
 
